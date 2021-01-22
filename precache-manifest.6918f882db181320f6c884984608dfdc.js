3626758e3525a43392596d7d63b619d35fdeb517
saurav <avisaurav485@gmail.com>
2021-01-22 01:25:42 -0600

    Updates

---
+++ precache-manifest.6918f882db181320f6c884984608dfdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8ef373d2d496bb51be80eae0263701",
+    "url": "/social-dashboard/index.html"
+  },
+  {
+    "revision": "5b9539619130a9dea0e8",
+    "url": "/social-dashboard/static/js/2.ff08da08.chunk.js"
+  },
+  {
+    "revision": "0e55c44b974638391e2284ee8b3abfdb",
+    "url": "/social-dashboard/static/js/2.ff08da08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478bbd35f7c7c82064d5",
+    "url": "/social-dashboard/static/js/main.75395170.chunk.js"
+  },
+  {
+    "revision": "c2a89a339a698fd04a6a",
+    "url": "/social-dashboard/static/js/runtime-main.4a0b533a.js"
+  }
+]);
